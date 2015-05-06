8298f4367864ecc24394058b36418d84059b84aa
Gabriel Carpenedo <cbgabe@gmail.com>
2015-05-06 14:16:45 -0300

    Adds validation and error message

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
-# import libs
+  # import libs
 # import routes
 
 require 'sinatra'
 require 'sinatra/asset_pipeline'
+
+MAIL_ATTRIBUTES = [:name, :mail, :phone, :budget]
+
+def invalid_attributes
+  attributes = []
+  MAIL_ATTRIBUTES.each do |attr|
+    attributes << attr if params[:message][attr].nil? || params[:message][attr].strip.empty?
+  end
+  attributes << :mail unless params[:message][:mail] && params[:message][:mail].match(/.+@.+\..+/)
+  attributes
+end
+
+def is_valid_mail?
+  invalid_attributes.empty? && not_spam?
+end
+
+def not_spam?
+  antispam = params[:message][:antispam]
+  spam = params[:message][:spam]
+  antispam == '' && spam == ''
+end
 
 class App < Sinatra::Base
   #set :assets_prefix, %w(assets assets/sass)
@@ -16,6 +37,7 @@ class App < Sinatra::Base
   end
 
   post '/contact' do
+    return redirect("/?errors=#{invalid_attributes.map(&:to_s).join(",")}") unless is_valid_mail?
     name = params[:message][:name]
     mail = params[:message][:mail]
     phone = params[:message][:phone]
